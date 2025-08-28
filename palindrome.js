e75bcd69940d26db22f881ceb45846fc01352651
sandra11223 <sp151048@gmail.com>
2025-08-28 13:38:39 +0530

    first commit

--- palindrome.js
+++ palindrome.js
@@ -1,13 +1,25 @@
-let str1 = 'listen';
-let str2 = 'silent';
-let a=str1.split("").sort().join("");
-let b=str2.split("").sort().join(""); 
-if (a==b) {
-    console.log("anagram");
-} else {
-    console.log("not anagram");
+let arr=['a','b','c','b','a'];
+let b=[...arr];
+console.log(arr);
+b.reverse();
+console.log(b);
+let flag;
+for(let i=0;i<arr.length;i++)
+{
+    if(arr[i]==b[i])
+    {
+        flag=1;
+    }
+    else
+    {
+        flag=0;
+    }
 }
-
-
-                  
-
+if(flag==1)
+{
+    console.log("palindrome");
+}
+else
+{
+    console.log("not palindrome");
+}
